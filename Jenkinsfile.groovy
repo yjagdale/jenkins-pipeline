1440835b49db987d358b5a187e47848228f925a5
Yashwant Jagdale <yashwant.jagdale@veritas.com>
2018-08-07 22:59:56 +0530

    Added missed function

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -187,3 +187,28 @@ def prepareExecution(String dirName, String gitUrl) {
        echo dirName + gitUrl
     }
 }
+
+def getDevVersion() {
+    def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+    def versionNumber
+    if (gitCommit == null) {
+        versionNumber = env.BUILD_NUMBER
+    } else {
+        versionNumber = gitCommit.take(8)
+    }
+    print 'build  versions...'
+    print versionNumber
+    return versionNumber
+}
+
+def getReleaseVersion() {
+    def pom = readMavenPom file: 'pom.xml'
+    def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+    def versionNumber
+    if (gitCommit == null) {
+        versionNumber = env.BUILD_NUMBER
+    } else {
+        versionNumber = gitCommit.take(8)
+    }
+    return pom.version.replace("-SNAPSHOT", ".${versionNumber}")
+}
